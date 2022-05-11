1fc14aae4369794f7a3525b5ffd46d6ccf3d5a80
Suraj Singh <surajsingh9090@gmail.com>
2022-05-11 17:04:08 +0530

    Configuration with DB and Running Envoirment for Spring Boot Application is DONE..!!

--- employee-management-test-web-app/src/main/java/com/test/employee/controller/EmployeeController.java
+++ employee-management-test-web-app/src/main/java/com/test/employee/controller/EmployeeController.java
@@ -1,11 +1,26 @@
 package com.test.employee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.test.employee.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
 	
+	//Injecting Service Class
+	@Autowired
+	private EmployeeService employeeService;
+	
 	//Display List of Employees
 	//Creating method handler for HOME Page (index.html) to display list of employees
 	
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		
+		model.addAttribute("listEmployees", employeeService.getAllEmployees());
+		return "index";
+	}
 }
