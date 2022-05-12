6dfa08318d236d36e2ba59ba4ff80c6d41363747
Suraj Singh <surajsingh9090@gmail.com>
2022-05-12 15:57:04 +0530

    Saving Employee Data Front-End Part is DONE..!!

--- employee-management-test-web-app/src/main/java/com/test/employee/controller/EmployeeController.java
+++ employee-management-test-web-app/src/main/java/com/test/employee/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.test.employee.model.Employee;
 import com.test.employee.service.EmployeeService;
 
 @Controller
@@ -18,9 +19,16 @@ public class EmployeeController {
 	//Creating method handler for HOME Page (index.html) to display list of employees
 	
 	@GetMapping("/")
-	public String viewHomePage(Model model) {
-		
+	public String viewHomePage(Model model) {	
 		model.addAttribute("listEmployees", employeeService.getAllEmployees());
 		return "index";
+	}
+	
+	@GetMapping("/showNewEmployeeForm")
+	public String showNewEmployeeForm(Model model) {
+		//Create Model attribute to bind from Data
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "new_employee";
 	}
 }
